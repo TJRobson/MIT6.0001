48f9b3886f05a90364b1b109e4caedbede77b332
TJRobson <thomas.j.robson@hotmail.com>
2017-06-13 01:48:51 +1200

    this function does not work, should have stuck with the original plan, woops

--- problem_set_4/ps4a.py
+++ problem_set_4/ps4a.py
@@ -20,21 +20,40 @@ def get_permutations(sequence):
     ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
 
     Note: depending on your implementation, you may return the permutations in
-    a different order than what is listed here.
+    a different order than what is listed here.    
     '''
+    permutations_list = []
+    
+    def perm_sec(string):
+        
+        reverse = string[::-1]
+        permutations_list.append(reverse)
+        first_letter = reverse[0]
+        cut_string = reverse[1:]
+        cut_reverse = cut_string[::-1]
+        the_next_string = first_letter+cut_reverse
+        permutations_list.append(the_next_string)
+        
+        if the_next_string == sequence:
+            return
+        else:
+            return perm_sec(the_next_string)
+    
+    perm_sec(sequence)
+      
+    return permutations_list      
 
-    pass #delete this line and replace with your code here
 
 if __name__ == '__main__':
 #    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+    print('for letters', get_permutations('a'))
     
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
 
